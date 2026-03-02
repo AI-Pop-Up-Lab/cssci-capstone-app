2a156bcad349ae666014e023283e62b595e2a4cc
br3nd0g <brendan.j.corcoran@gmail.com>
2026-03-02 19:22:40 +0100

    folder added as normal directory

---
+++ ai-popup-lab-application/src/landingPage.js
@@ -0,0 +1,15 @@
+import PopupDisclaimer from './popupDisclaimer';
+
+import './landingPage.css';
+
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <p>hey</p>
+      <PopupDisclaimer />
+    </div>
+  );
+}
+
+export default LandingPage;
