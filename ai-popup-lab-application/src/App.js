3cd89e31482cfa0beedf064cf3b62de45f421024
br3nd0g <brendan.j.corcoran@gmail.com>
2026-03-03 14:38:53 +0100

    Added pycache files to gitignore and created react page routing

---
+++ ai-popup-lab-application/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import LandingPage from './pages/landingPage';
+
+function App() {
+  return (
+    <Router>
+      {/* <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/other-page">Page 2</Link></li>
+          <li><Link to="/other-other-page">Page 3</Link></li>
+        </ul>
+      </nav> 
+        This would be how the navigation would work once multiple pagers exist
+      */}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- ai-popup-lab-application/src/modules/popupDisclaimer.js
+++ ai-popup-lab-application/src/modules/popupDisclaimer.js
@@ -1,6 +1,6 @@
 import { useRef, useEffect, useState } from "react";
 
-import closeCross from './assets/images/closeCross.png';
+import closeCross from '../assets/images/closeCross.png';
 import './popupDisclaimer.css';
 
 function PopupDisclaimer() {
