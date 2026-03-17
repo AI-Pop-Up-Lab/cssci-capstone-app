028e4fa0298c344c79e5df70f4c4bce9c9255cea
br3nd0g <brendan.j.corcoran@gmail.com>
2026-03-17 20:35:51 +0100

    restructured files

---
+++ ai-popup-lab-application/src/modules/personaChatExample.js
@@ -0,0 +1,53 @@
+import './personaChatExample.css';
+
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import personaPic_1 from '../assets/svgs/personaPic_1.svg'
+import personaPic_2 from '../assets/svgs/personaPic_2.svg'
+
+function PersonaChatExample({includeLink}) {
+  return (
+    <div className="PersonaChatExample unbounded-weight300">
+        <div id='land-expl-pers-top'>
+
+          <div id="example-persona-card">
+            <img src={personaPic_1}></img>
+            <p className='unbounded-weight400' id="example-persona-name">Synthetic Persona</p>
+            <p className='example-persona-attr'>Age: 26</p>
+            <p className='example-persona-attr'>Education: High school</p>
+            <p className='example-persona-attr'>Occupation: Unemployed</p>
+          
+            <div id="persona-warning-hover">
+              <p className='unbounded-weight400'>!</p>
+            </div>
+
+            <div id="persona-warning-overlay">
+              <p className='unbounded-weight400'>THESE PERSONAS ARE  <span style={{fontWeight: 600}}>NOT REAL HUMAN BEINGS</span>, AND NO PERSONALLY IDENTIFIABLE DATA OF LIVING INDIVIDUALS IS USED. </p>
+            </div>
+          </div>
+
+          <div id="example-persona-chat">
+            <div id='example-user-message-container'>
+              <div id="example-user-message">
+                <p>How do you feel about current economic situation in The Netherlands?</p>
+                <div id="example-user-message-bubbletick"></div>
+              </div>
+            </div>
+            <div id='example-response-message-container'>
+              <div id="example-response-message">
+                <p>Honestly, it feels pretty stressful right now. As someone who’s unemployed, it seems like everything is getting more expensive while finding stable work is still tough. I worry a lot about paying rent and basic things like groceries. The Netherlands is still a good country in many ways, but the economic situation makes it feel uncertain and a bit overwhelming sometimes.</p>
+                <div id="example-response-message-bubbletick"></div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div id='land-expl-pers-bottom' className={includeLink ? '' : 'dontShowPersonaLink'}>
+          <Link to="/personas"><button className='unbounded-weight300'>EXPLORE MORE PERSONAS <span>{'\u{1F782}'}</span></button></Link>
+        </div>
+    </div>
+  );
+}
+
+export default PersonaChatExample;
--- ai-popup-lab-application/src/pages/landingPage.js
+++ ai-popup-lab-application/src/pages/landingPage.js
@@ -2,10 +2,9 @@
 
 import './landingPage.css';
 import chartGraphic from '../assets/images/chartGraphic.png'
-import personaPic_1 from '../assets/svgs/personaPic_1.svg'
-import personaPic_2 from '../assets/svgs/personaPic_2.svg'
 
 import PollingResults from '../modules/polling_results_modules/pollingResults';
+import PersonaChatExample from '../modules/personaChatExample';
 
 function LandingPage() {
   return (
@@ -19,49 +18,9 @@ function LandingPage() {
     
       <PollingResults />
 
-      <div id='landing-explore-personas'>
+      <PersonaChatExample includeLink={true} />
 
-        <div id='land-expl-pers-top'>
-
-          <div id="example-persona-card">
-            <img src={personaPic_1}></img>
-            <p className='unbounded-weight400' id="example-persona-name">Synthetic Persona</p>
-            <p className='example-persona-attr'>Age: 26</p>
-            <p className='example-persona-attr'>Education: High school</p>
-            <p className='example-persona-attr'>Occupation: Unemployed</p>
-          
-            <div id="persona-warning-hover">
-              <p className='unbounded-weight400'>!</p>
-            </div>
-
-            <div id="persona-warning-overlay">
-              <p className='unbounded-weight400'>THESE PERSONAS ARE  <span style={{fontWeight: 600}}>NOT REAL HUMAN BEINGS</span>, AND NO PERSONALLY IDENTIFIABLE DATA OF LIVING INDIVIDUALS IS USED. </p>
-            </div>
-          </div>
-
-          <div id="example-persona-chat">
-            <div id='example-user-message-container'>
-              <div id="example-user-message">
-                <p>How do you feel about current economic situation in The Netherlands?</p>
-                <div id="example-user-message-bubbletick"></div>
-              </div>
-            </div>
-            <div id='example-response-message-container'>
-              <div id="example-response-message">
-                <p>Honestly, it feels pretty stressful right now. As someone who’s unemployed, it seems like everything is getting more expensive while finding stable work is still tough. I worry a lot about paying rent and basic things like groceries. The Netherlands is still a good country in many ways, but the economic situation makes it feel uncertain and a bit overwhelming sometimes.</p>
-                <div id="example-response-message-bubbletick"></div>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-        <div id='land-expl-pers-bottom'>
-          <button className='unbounded-weight300'>EXPLORE MORE PERSONAS <span>{'\u{1F782}'}</span></button>
-        </div>
-
-      </div>
-    
+  
     </div>
   );
 }
