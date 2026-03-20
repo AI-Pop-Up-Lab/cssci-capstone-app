9735986c3b1f820b831f6e7050d1679647d15c2d
br3nd0g <brendan.j.corcoran@gmail.com>
2026-03-20 15:53:42 +0100

    one more try

--- ai-popup-lab-backend/main.py
+++ ai-popup-lab-backend/main.py
@@ -15,7 +15,8 @@ origins = [
     # Local development URLs, for use with React application
     "http://localhost:3000",  
     "http://127.0.0.1:3000",
-    "https://ai-pollster.vercel.app/"
+    "https://ai-pollster.vercel.app"
+    
     # production frontend url to be added here when deployed
 ]
 
