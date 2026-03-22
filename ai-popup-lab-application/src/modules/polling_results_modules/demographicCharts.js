27b73db2b60f9812e1152bcb0a44f9c541cb930e
br3nd0g <brendan.j.corcoran@gmail.com>
2026-03-22 20:00:56 +0100

    styling changes for tablet

--- ai-popup-lab-application/src/modules/polling_results_modules/demographicCharts.js
+++ ai-popup-lab-application/src/modules/polling_results_modules/demographicCharts.js
@@ -39,8 +39,8 @@ function DemographicCharts({ pollingData, country }) {
       
     let svgW = 820
 
-    if(window.innerWidth <= 900){
-      svgW = 600;
+    if(window.innerWidth <= 930){
+      svgW = 570;
     }
 
     const margin = { top: 30, right: 20, bottom: 80, left: 50 };
