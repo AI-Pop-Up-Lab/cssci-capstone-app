16f718b8df465aaed23b342b94dd1f6453bb72e5
br3nd0g <brendan.j.corcoran@gmail.com>
2026-05-23 18:06:49 +0200

    startup commands modified in docker image and old .sh file

--- ai-popup-lab-backend/startup.sh
+++ ai-popup-lab-backend/startup.sh
@@ -1,3 +1,6 @@
+# OLD STARTUP COMMAND THAT EXISTED FOR AZURE + GITHUB ACTIONS CONTINUOUS DEPLOYMENT.
+# now docker is used so the requirements are in the docker image
+
 #!/bin/bash
-pip install -r requirements.txt
-gunicorn -w 1 -k uvicorn.workers.UvicornWorker main:app --timeout 120 --threads 12
+# pip install -r requirements.txt
+# gunicorn -w 1 -k uvicorn.workers.UvicornWorker main:app --timeout 120 --threads 12
